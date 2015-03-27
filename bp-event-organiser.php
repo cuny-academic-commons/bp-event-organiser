85c68d744f45dbb2d09f99213c574b4ea217dbb7
r-a-y <r-a-y@gmx.com>
2015-03-27 09:51:29 -0700

    Remove JS.
    Since EO v2.13.0, we no longer need to add JS to dynamically set the
    group ID or displayed user ID.  We can use the new
    'eventorganiser_fullcalendar_query' filter to add our custom parameters.

--- bp-event-organiser.php
+++ bp-event-organiser.php
@@ -92,9 +92,6 @@ class BuddyPress_Event_Organiser {
 		// use translation files
 		$this->enable_translation();
 
-		// register any public scripts
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-
 		// add action for CBOX theme compatibility
 		add_action( 'wp_head', array( $this, 'cbox_theme_compatibility' ) );
 
@@ -175,36 +172,6 @@ class BuddyPress_Event_Organiser {
 			dirname( plugin_basename( __FILE__ ) ) . '/languages/'
 
 		);
-
-	}
-
-
-
-	/**
-	 * @description: add our global scripts
-	 * @return nothing
-	 */
-	public function enqueue_scripts() {
-
-		// enqueue script to amend EO Calendar
-		wp_enqueue_script(
-
-			'bp_event_organiser_js',
-			BUDDYPRESS_EVENT_ORGANISER_URL . 'assets/js/bp-event-organiser.js',
-			array( 'jquery' ),
-			BUDDYPRESS_EVENT_ORGANISER_VERSION,
-			true // in footer
-
-		);
-
-		// get vars
-		$vars = array(
-			'group_id' => bp_get_current_group_id(),
-			'displayed_user_id' => bp_displayed_user_id(),
-		);
-
-		// localise with wp function
-		wp_localize_script( 'bp_event_organiser_js', 'BpEventOrganiserSettings', $vars );
 
 	}
 
